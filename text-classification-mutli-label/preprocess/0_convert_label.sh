fc6b0a08e49ab3864fa49f795b38a8cdc4e1c87d
Zhang_Shu <35102348+littletiger0712@users.noreply.github.com>
2019-03-05 20:08:55 +0800

    init
    init the project

---
+++ text-classification-mutli-label/preprocess/0_convert_label.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+python 1_get_sentence_content.py
+python 4_generate_dataformat.py
---
+++ text-classification-mutli-label/preprocess/2_sentenceCut_jieba.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import json
+import os
+import sys
+sys.path.append('../')
+import jieba
+
+from config import base_dir
+
+data_dir = os.path.join(base_dir, 'data')
+src = 'content'
+target = 'jieba'
+
+process_name = ['train', 'valid', 'test']
+
+def function():
+    stopwords = list(map(lambda x: x[:-1], open(os.path.join(data_dir, 'stop_words.txt'))))
+
+    for name in process_name:
+        srcfile = open(os.path.join(
+            data_dir, 'data_{}_{}.json'.format(name, src)))
+        targetfile = open(os.path.join(
+            data_dir, 'data_{}_{}.json'.format(name, target)), 'w')
+
+        for line in srcfile:
+            x_cut = list(filter(lambda x: x not in stopwords, jieba.cut(line)))
+            targetfile.write(json.dumps(x_cut)+'\n')
+        srcfile.close()
+        targetfile.close()
+
+if __name__ == "__main__":
+    function()
