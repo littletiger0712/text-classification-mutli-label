ba9a82fc815d9a7c0cb1e7ceeba84c06fa226818
littletiger0712 <zhangshu960712@163.com>
2019-06-08 20:44:00 +0800

    save

--- zs_run.sh
+++ zs_run.sh
@@ -1,21 +1,21 @@
 head -q -n 10000 data/data_train_fasttext.txt.ori > data/data_train_fasttext.txt
-nohup python run_fasttext.py > fasttext_1w_ori.log 2>&1 &
-nohup python run_cnn.py > cnn_1w_ori.log 2>&1 &
+nohup python run_fasttext.py train > fasttext_1w_ori.log 2>&1 
+nohup python run_cnn.py train > cnn_1w_ori.log 2>&1 
 head -q -n 10000 data/data_train_fasttext.txt.ori data/data_train_fasttext.txt.2 data/data_train_fasttext.txt.3 data/data_train_fasttext.txt.4 > data/data_train_fasttext.txt
-nohup python run_fasttext.py > fasttext_1w_aug.log 2>&1 &
-nohup python run_cnn.py > cnn_1w_aug.log 2>&1 &
+nohup python run_fasttext.py train > fasttext_1w_aug.log 2>&1 
+nohup python run_cnn.py train > cnn_1w_aug.log 2>&1 
 
 head -q -n 50000 data/data_train_fasttext.txt.ori > data/data_train_fasttext.txt
-nohup python run_fasttext.py > fasttext_5w_ori.log 2>&1 &
-nohup python run_cnn.py > cnn_5w_ori.log 2>&1 &
+nohup python run_fasttext.py train > fasttext_5w_ori.log 2>&1 
+nohup python run_cnn.py train > cnn_5w_ori.log 2>&1 
 head -q -n 50000 data/data_train_fasttext.txt.ori data/data_train_fasttext.txt.2 data/data_train_fasttext.txt.3 data/data_train_fasttext.txt.4 > data/data_train_fasttext.txt
-nohup python run_fasttext.py > fasttext_5w_aug.log 2>&1 &
-nohup python run_cnn.py > cnn_5w_aug.log 2>&1 &
+nohup python run_fasttext.py train > fasttext_5w_aug.log 2>&1 
+nohup python run_cnn.py train > cnn_5w_aug.log 2>&1 
 
 head -q -n 100000 data/data_train_fasttext.txt.ori > data/data_train_fasttext.txt
-nohup python run_fasttext.py > fasttext_10w_ori.log 2>&1 &
-nohup python run_cnn.py > cnn_10w_ori.log 2>&1 &
+nohup python run_fasttext.py train > fasttext_10w_ori.log 2>&1 
+nohup python run_cnn.py train > cnn_10w_ori.log 2>&1 
 head -q -n 100000 data/data_train_fasttext.txt.ori data/data_train_fasttext.txt.2 data/data_train_fasttext.txt.3 data/data_train_fasttext.txt.4 > data/data_train_fasttext.txt
-nohup python run_fasttext.py > fasttext_10w_aug.log 2>&1 &
-nohup python run_cnn.py > cnn_10w_aug.log 2>&1 &
+nohup python run_fasttext.py train > fasttext_10w_aug.log 2>&1 
+nohup python run_cnn.py train > cnn_10w_aug.log 2>&1 
 
