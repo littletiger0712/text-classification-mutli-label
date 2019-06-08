eeb8dfb0035514004d007966a44ddf9352f2e1d9
littletiger0712 <zhangshu960712@163.com>
2019-06-08 20:22:25 +0800

    save

---
+++ zs_run.sh
@@ -0,0 +1,21 @@
+head -10000 data/data_train_fasttext.txt.ori > data/data_train_fasttext.txt
+nohup python run_fasttext.py > fasttext_1w_ori.log 2>&1 &
+nohup python run_cnn.py > cnn_1w_ori.log 2>&1 &
+head -10000 data/data_train_fasttext.txt.ori head -10000 data/data_train_fasttext.txt.2 head -10000 data/data_train_fasttext.txt.3 head -10000 data/data_train_fasttext.txt.4 > data/data_train_fasttext.txt
+nohup python run_fasttext.py > fasttext_1w_aug.log 2>&1 &
+nohup python run_cnn.py > cnn_1w_aug.log 2>&1 &
+
+head -50000 data/data_train_fasttext.txt.ori > data/data_train_fasttext.txt
+nohup python run_fasttext.py > fasttext_5w_ori.log 2>&1 &
+nohup python run_cnn.py > cnn_5w_ori.log 2>&1 &
+head -50000 data/data_train_fasttext.txt.ori head -50000 data/data_train_fasttext.txt.2 head -50000 data/data_train_fasttext.txt.3 head -50000 data/data_train_fasttext.txt.4 > data/data_train_fasttext.txt
+nohup python run_fasttext.py > fasttext_5w_aug.log 2>&1 &
+nohup python run_cnn.py > cnn_5w_aug.log 2>&1 &
+
+head -100000 data/data_train_fasttext.txt.ori > data/data_train_fasttext.txt
+nohup python run_fasttext.py > fasttext_10w_ori.log 2>&1 &
+nohup python run_cnn.py > cnn_10w_ori.log 2>&1 &
+head -100000 data/data_train_fasttext.txt.ori head -100000 data/data_train_fasttext.txt.2 head -100000 data/data_train_fasttext.txt.3 head -100000 data/data_train_fasttext.txt.4 > data/data_train_fasttext.txt
+nohup python run_fasttext.py > fasttext_10w_aug.log 2>&1 &
+nohup python run_cnn.py > cnn_10w_aug.log 2>&1 &
+
